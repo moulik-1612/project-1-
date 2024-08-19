4ef4679bca93320fce496ccc9621d15a07c06e43
moulik-1612 <mouliktiwari1612@gmail.com>
2024-08-19 16:37:17 +0530

    Adding more js logics

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-let select = document.querySelector(".add-product select");
+let select = document.querySelector("#pro");
 let qty = document.querySelector("#quantity");
 let show_price = document.querySelector("#display-price");
 let btn = document.querySelector("button");
 
+let op1 = document.querySelector('#opt1');
+let op2 = document.querySelector('#opt2');
+let op3 = document.querySelector('#opt3');
 
 let x = document.querySelector("#textarea");
 let x1 = document.querySelector("#order-textarea");
@@ -26,11 +29,20 @@ let order_info = [];
 
 
 btn.addEventListener("click", () => {
+    if(qty.value == ''){
+        alert('please fill the quantity');
+    }else {
 show_price.value = select.value * qty.value;
-str = `Quantity = ${qty.value}\nPrice = ${show_price.value}\n\n`;
+str = `product = ${select.options[select.selectedIndex].innerHTML}\nQuantity = ${qty.value}\nPrice = ${show_price.value}\n\n`;
 order_info.push(str);
 x.value += order_info[n];
 n++;
+    }
+    select.addEventListener('click', ()=> {
+        if(name.value == ''){
+            alert('please fill the contact info and submit your order');
+        }
+    })
 })
 
 
@@ -39,11 +51,15 @@ let customer_inf0 = [];
 let m = 0;
 
 btn1.addEventListener("click", () => {
-    Str = `Name = ${name.value}\nEmail Id = ${email.value}\nNumber = ${number.value}`;
-    customer_inf0.push(Str);
-    x1.value += order_info[m];
-    x1.value += customer_inf0[m];
-    m++;
+    if(name.value == '' && email.value == '' && number.value == ''){
+        alert('please fill contact information');
+    }else {
+        Str = `Name = ${name.value}\nEmail Id = ${email.value}\nNumber = ${number.value}`;
+        customer_inf0.push(Str);
+        x1.value += order_info[m];
+        x1.value += customer_inf0[m];
+        m++;
+    }
 })
 
 select.addEventListener("change", () => {
@@ -54,7 +70,7 @@ select.addEventListener("change", () => {
 function removeData () {
     qty.value = "";
     show_price.value = "";
-    name.value = "";
+    // name.value = "";
     email.value = "";
     number.value = "";
     x1.value = "";
